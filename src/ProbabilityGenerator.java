b48a2b788c2f3b47e6b9d488b9f8464ec117aed5
Tianyu Liang <tianyu@wireless-10-148-111-146.public.utexas.edu>
2018-05-13 22:06:17 -0500

    use double to help with int overflow

--- src/ProbabilityGenerator.java
+++ src/ProbabilityGenerator.java
@@ -1,35 +1,69 @@
 import java.util.*;
-
+import java.math.BigInteger;
 
 
 public class ProbabilityGenerator
 {
     public static final int NUMBER_OF_DICE = 4;
-    public static final int RANGE = 20;
+    public static final int RANGE = 6;
     
     
     public static void main(String[] args) {
         ProbabilityGenerator p = new ProbabilityGenerator();
         // order of inputs: number of die, range of each dice, and the target sum
-        long total = 1;
+        double total = 1;
         for(int i = 0; i < NUMBER_OF_DICE; i++) {
             total = total * RANGE;
         }
-        long result = 0;
-        for(int i = 4; i < 81; i++) {
-            result = result + p.numberOfArrangements(NUMBER_OF_DICE, RANGE, i);
-        }
-        // result = result + p.numberOfArrangements(NUMBER_OF_DICE, RANGE, 11);
-        System.out.println("VERIFICATION: total probability adds up to: " + result + " / " + total);    
         
+//        double result = 0;
+//        for(int i = 4; i < 25; i++) {
+//            result = result + p.numberOfArrangements(NUMBER_OF_DICE, RANGE, i);
+//        }
+//        // result = result + p.numberOfArrangements(NUMBER_OF_DICE, RANGE, 350);
+//        System.out.println("VERIFICATION: total probability adds up to: " + result + " / " + total);  
+        int [][] A = new int[32][32];
+        int [][] B = new int[32][32];
+     
+        for(int i = 0; i < 32; i++) {
+            for(int j = 0; j < 32; j++) {
+                A[i][j] = (int)(Math.random() * 1000);
+                
+            }
+        }
+        for(int i = 0; i < 32; i++) {
+            for(int j = 0; j < 32; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        p.matrix(A, B);
+        System.out.println();
+        for(int i = 0; i < 32; i++) {
+            for(int j = 0; j < 32; j++) {
+                System.out.print(B[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        System.out.println();
+        for(int i = 0; i < 32; i++) {
+            for(int j = 0; j < 32; j++) {
+                if(A[i][j] != B[j][i]) {
+                    System.out.println("i: " + i + "j: gooby" + j);
+                }
+            }
+            
+        }
     }
 
     
-    long[] factorial;
-    public long numberOfArrangements(int numOfDie, int range, int target) {
-        factorial = new long[numOfDie + 1];
+    double[] factorial;
+    public double numberOfArrangements(int numOfDie, int range, int target) {
+        factorial = new double[numOfDie + 1];
         factorial();
-        ArrayList<Long> track = new ArrayList<Long>();
+        ArrayList<Double> track = new ArrayList<Double>();
         HashMap<Integer, Integer> repeats = new HashMap<>();
         int[] numbers = new int[numOfDie * range];
         for(int i = 0; i < range; i++) {
@@ -42,20 +76,20 @@ public class ProbabilityGenerator
         System.out.println("number of die: " + numOfDie + ", range of dice: " + range + ", sum target: " + target);
         // unique arrangements (set)
         System.out.println("number of unique arrangements: " + arrangementHelper(numbers, 0, numOfDie, target, 1, track, repeats));
-        long result = 0;
+        double result = 0;
         for(int i = 0; i < track.size(); i++) {
             // System.out.println(track.get(i));
             result = result + track.get(i);
         }
-        long total = 1;
+        double total = 1;
         for(int i = 0; i < numOfDie; i++) {
             total = total * range;
         }
-        System.out.println(result + " / " + total);
+        System.out.println("probability: " + result + " / " + total);
         return result;
     }
     // numbers array represent all possible dice numbers
-    private int arrangementHelper(int[] numbers, int index, int numOfDie, int target, int depth, ArrayList<Long> track, HashMap<Integer, Integer> repeats) {
+    private int arrangementHelper(int[] numbers, int index, int numOfDie, int target, int depth, ArrayList<Double> track, HashMap<Integer, Integer> repeats) {
         if(depth == numOfDie - 1) {
             int i = index;
             int k = numbers.length - 1;
@@ -135,7 +169,7 @@ public class ProbabilityGenerator
 
     
     // set up the map for probability calculations and store value in arraylist
-    private void mapSetUp(HashMap<Integer, Integer> repeats, int i, int j, ArrayList<Long> track) {
+    private void mapSetUp(HashMap<Integer, Integer> repeats, int i, int j, ArrayList<Double> track) {
         Integer temp = repeats.get(i);
         if(temp != null) {
             repeats.put(i, temp + 1);
@@ -150,7 +184,7 @@ public class ProbabilityGenerator
             repeats.put(j, 1);
         }
         Set<Integer> set = repeats.keySet();
-        long result = factorial[factorial.length - 1];
+        double result = factorial[factorial.length - 1];
         // System.out.println(repeats);
         for(int a: set) {
             
@@ -219,5 +253,61 @@ public class ProbabilityGenerator
             }
      
      */
-    
+   
+    public void matrix(int[][] A, int[][] B) {
+        int i, j, tmp, row, col, a, b;
+        
+
+        for(col = 0; col < 32; col = col + 8){
+            for(row = 0; row < 32; row = row + 4){
+                for (i = row; i < row + 4; i = i + 1) {
+                    //index = -1;
+                    for (j = col; j < col + 8; j = j + 1) {
+                        //System.out.println("j " + j + " i dsa" + i);
+                        if(j < col + 4){
+                            //System.out.println(" i " + i + " j " + j);
+                            B[i][j] = A[j][i];
+                        }else{
+                            B[j][i] = A[j][i];
+                        }
+             
+                     
+                    }
+                    // if(index > -1){
+                    //  B[index][index] = temp;
+                    // }
+                }
+                i = row;
+                j = col + 4;
+                
+                if(i == j) {
+                    for (i = row; i < row + 4; i = i + 1) {
+                        for(j = i; j < col + 8; j++){
+                            System.out.println("i : " + i + " j: " + j + " gooby");
+                            a = B[j][i];
+                            B[j][i] = B[i][j];
+                            B[i][j]= a;
+                            
+                        }
+                    }
+                }else {
+                    for (i = row; i < row + 4; i = i + 1) {
+                        for(j = col + 4; j < col + 8; j++){
+                            System.out.println("i : " + i + " j: " + j);
+                            a = B[j][i];
+                            b = B[i][j];
+                            B[i][j] = a;
+                            B[j][i] = b;
+                            
+                        }
+                    }
+                }
+                
+                
+             
+            }
+
+        }
+
+    }
 }
